f51ecbc82c5348efe5e5956b86b6de0d6e0907df
Radiant Fadilah <radiantfadilah0@gmail.com>
2025-03-14 15:53:22 +0700

    [add] reducer & api service

--- redux/index.ts
+++ redux/index.ts
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import audioReducer from "./slices/audioSlice";
+import authReducer from "./slices/authSlice";
+import { apiService } from "./api/apiSlice";
 
 export const store = configureStore({
   reducer: {
+    auth: authReducer,
     audio: audioReducer,
+    [apiService.reducerPath]: apiService.reducer,
   },
+  middleware: (defaultMiddleware) =>
+    defaultMiddleware().concat(apiService.middleware),
 });
